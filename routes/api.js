4fc0407fe4a44fbd2ce42025f5b937b683d85cf1
duxard <duxard@gmail.com>
2019-04-04 22:00:11 +0300

    heroku: minor refactor

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,7 @@ router.get('/todo', (req, res) => {
         res.send(data);
     });
 });
-
+/*
 router.post('/todo', (req, res) => {
     Todo(req.body).save((err, data) => {
         if(err) throw new Error(err);
@@ -26,36 +26,5 @@ router.delete('/todo/:item', (req, res) => {
         res.json(data);
     });
 });
-
-module.exports = router;
-/*
-//get a list of animals from the DB
-router.get('/animals', function(req, res, next){
-    Animals.find({}).then(function(items){
-        res.send(items);
-    });
-});
-
-//add a new animal to the db
-router.post('/animals', function(req, res, next){
-	console.log(req.body);
-    Animals.create(req.body).then(function(item){
-        res.send(item);
-    }).catch(next);
-});
-
-//delete an animal from the db
-router.delete('/animals/:id', function(req, res, next){
-    Animals.findByIdAndRemove({_id: req.params.id}).then(function(item){
-        console.log("Item has been deleted: ", item);
-        res.send(item);
-    }).catch(next);
-});
-
-//get a list of animals from the DB
-router.get('/animals/test', function(req, res, next){
-//    res.send(JSON.stringify(testObject));
-    res.send(testObject);
-
-});
 */
+module.exports = router;
