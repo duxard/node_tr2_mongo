d13e45b0fc21f9c2c9ec823c5a0f4dfff44c6173
duxard <duxard@gmail.com>
2019-04-08 19:53:14 +0300

    add POST request

--- routes/api.js
+++ routes/api.js
@@ -13,14 +13,14 @@ router.get('/todo', (req, res) => {
         res.send(data);
     });
 });
-/*
+
 router.post('/todo', (req, res) => {
     Todo(req.body).save((err, data) => {
         if(err) throw new Error(err);
         res.json(data);
     });
 });
-
+/*
 router.delete('/todo/:item', (req, res) => {
     Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove((err, data) => {
         if(err) throw new Error(err);
