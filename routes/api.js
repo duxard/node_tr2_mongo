8d7654ff4636705ecfc46430af04f8b7508e8652
duxard <duxard@gmail.com>
2019-04-09 20:18:36 +0300

    DELETE refactor

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,8 @@ const express = require('express'),
       Todo = require('../models/Todo.js');
 	  
 router.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
+	//res.header("Access-Control-Allow-Origin", "*");
+	res.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
