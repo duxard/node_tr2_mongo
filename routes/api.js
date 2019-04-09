2742ea34c3fe8a44625765b63b41b377ad59afa4
duxard <duxard@gmail.com>
2019-04-09 21:30:56 +0300

    DELETE refactor2

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,7 @@ const express = require('express'),
 router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	//res.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
