0bfeb9ed2cdb434da0d9f51c5d366c6750da8e80
Aleksandr <duxard@gmail.com>
2019-07-15 11:49:36 -0400

    add Post model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 REST endpoint
 https://asta-web-1.herokuapp.com/
+
+To use nodemon:
+1) Install it globally
+npm install -g nodemon
+
+2) Run nodemon:
+nodemon [app_name]
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express'),
       router = express.Router(),
-      Todo = require('../models/Todo.js');
-	  
+      Todo = require('../models/Todo.js'),
+      Post = require('../models/Post.js');
+
 router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	//res.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -9,7 +10,7 @@ router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
-      
+
 router.get('/', (req, res, next) => {
     res.send("API root page...");
 });
@@ -30,6 +31,20 @@ router.post('/todo', (req, res) => {
 
 router.delete('/todo/:id', (req, res) => {
     Todo.find({_id: req.params.id}).remove((err, data) => {
+        if(err) throw new Error(err);
+        res.json(data);
+    });
+});
+
+router.get('/post', (req, res) => {
+    Post.find({}, (err, data) => {
+        if(err) throw new Error(err);
+        res.send(data);
+    });
+});
+
+router.post('/post', (req, res) => {
+    Post(req.body).save((err, data) => {
         if(err) throw new Error(err);
         res.json(data);
     });
