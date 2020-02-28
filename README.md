c15d38bd1cafef4c10c38c02605d44e92cf4ddc3
duxard <duxard@gmail.com>
2020-02-28 23:01:17 +0300

    modify readme

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-REST endpoint
-https://asta-web-1.herokuapp.com/
+REST endpoint  
+https://asta-web-1.herokuapp.com/api/
 
-To use nodemon:
-1) Install it globally
+To use nodemon:  
+1) Install it globally  
 npm install -g nodemon
 
-2) Run nodemon:
-nodemon [app_name]
+2) Run nodemon:  
+nodemon [app_name]  
+nodemon server.js
+
+3) Deploy heroku  
+git push heroku [branch]
+
+4) To restart app (heroku CLI is required):  
+heroku restart -a [app_name]  
+heroku restart -a asta-web-1
+
+5) Heroku app logs:  
+heroku logs --tail
--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,8 @@ router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-control, Content-Type");
-  res.header("Access-Control-Max-Age","1728000");
-  res.header("Access-Control-Allow-Credentials","true");
+    res.header("Access-Control-Max-Age","1728000");
+    res.header("Access-Control-Allow-Credentials","true");
 	next();
 });
 
